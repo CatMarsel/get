34e7c36a04fdc34f6378aa735414daf66291b71b
Maksim <mkuzin2007@gmail.com>
2025-09-25 16:31:40 +0300

    Task 8

---
+++ get-led/bin-counter.py
@@ -0,0 +1,35 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+leds=[16,12,25,17,27,23,22,24]
+up=9
+dw=10
+st=0.2
+GPIO.setup(leds,GPIO.OUT)
+GPIO.setup(up,GPIO.IN)
+GPIO.setup(dw, GPIO.IN)
+
+GPIO.output(leds, 0)
+
+num = 0
+
+def dec2bin(value):
+    return [int(element) for element in bin(value)[2:].zfill(8)]
+
+while True:
+    if GPIO.input(up):
+        num=num+1
+        if num>=256 or num<0:
+            num=0
+        print(num, dec2bin(num))
+        time.sleep(st)
+    if GPIO.input(dw):
+        num=num-1
+        if num>=256 or num<0:
+            num=0
+        print(num, dec2bin(num))
+        time.sleep(st)
+    if num>256 or num<0:
+        num=0
+
+    GPIO.output(leds, dec2bin(num))
